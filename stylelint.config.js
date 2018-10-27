be2ea872ce9175f9ca560d0c304054255ea250ee
dudarmykola <dudar.mykola.stepan@gmail.com>
2018-10-27 20:11:39 +0200

    Added stylelint file's rule

--- stylelint.config.js
+++ stylelint.config.js
@@ -10,6 +10,9 @@ module.exports = {
       ignorePseudoClasses: ["global"]
     }],
     "selector-max-id": 0,
-    "selector-class-pattern": "^([a-z][a-z0-9]*)(-[a-z0-9]+)*$"
+    "selector-class-pattern": "^([a-z][a-z0-9]*)(-[a-z0-9]+)*$",
+    "at-rule-no-unknown": [true, {
+      "ignoreAtRules": ["function", "if", "each", "include", "mixin"]
+    }]
   }
 };
